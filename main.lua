fc644ff8ac895639f225d2f254bbc703b786b92b
Hobmoves <holberthobmoves@gmail.com>
2026-06-03 18:31:31 -0400

    test push

--- main.lua
+++ main.lua
@@ -1,9 +1,5 @@
 -- holbenchat-counter — HolbenChat Bot
 
-function events.init()
-  -- runs once when bot loads
-end
-
-function events.command.ping(ctx)
-  ctx.reply("pong! 🏓")
+function events.command.fruit(ctx)
+  ctx.reply("ping! 🏓")
 end
